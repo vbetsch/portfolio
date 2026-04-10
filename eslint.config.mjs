c3cafc3695e2c07e69c7d5270cea421f0838d750
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-04-10 18:31:44 +0200

    feat: Import and use lib-config-ts (#25)
    * feat: import lib-config-ts templates
    
    * feat: install lib-config-ts packages
    
    * feat: complete package.json
    
    * feat: add tsc check command
    
    * feat: sort package.json
    
    * feat: update lock file
    
    * feat: configure prettier
    
    * feat: configure eslint
    
    * feat: configure jest
    
    * feat: configure tsconfig
    
    * fix: extends order
    
    * feat: configure tsconfig build
    
    * feat: name start npm commands
    
    * style: format with prettier
    
    * feat: install jest dependencies
    
    * test: create one
    
    * feat: install eslint dependencies
    
    * fix: move @astrojs/check in devDependencies
    
    * fix: eslint config
    
    * refactor: optimize eslint conf
    
    * fix: eslint astro check error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,53 @@
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import baseVbetsch from '@vbetsch/config-eslint/base';
+import jsxVbetsch from '@vbetsch/config-eslint/jsx';
+import namingVbetsch from '@vbetsch/config-eslint/naming';
+import prettierVbetsch from '@vbetsch/config-eslint/prettier';
+import strictVbetsch from '@vbetsch/config-eslint/strict';
 
-export default tseslint.config(
-  [
-    {
-      ignores: [
-        '.astro/',
-      ],
-    },
+export default [
+  ...baseVbetsch,
+  strictVbetsch,
+  namingVbetsch,
+  jsxVbetsch,
+  ...prettierVbetsch,
 
-    {
-      languageOptions: {
-        parserOptions: {
-          project: './tsconfig.json',
-          extraFileExtensions: ['.astro'],
-        },
+  /* IMPORTANT: You have to add these lines only for the strict module ! */
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-    {
-      files: ['**/*.astro'],
-      languageOptions: {
-        parser: eslintPluginAstro.parser,
-        parserOptions: {
-          parser: tseslint.parser,
-        },
+  },
+
+  /* --- Overrides --- */
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: ['.astro/'],
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: eslintPluginAstro.parser,
+      parserOptions: {
+        extraFileExtensions: ['.astro'],
+        parser: tseslint.parser,
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-  ],
-);
+  },
+  // {
+  //   settings: {
+  //     'import/resolver': {
+  //       typescript: {
+  //         alwaysTryTypes: true,
+  //         project: './tsconfig.json',
+  //       },
+  //     },
+  //   },
+  // }
+];
---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+describe('config', () => {
+  it('should be OK', () => {
+    expect(true).toBeTruthy();
+  });
+});
