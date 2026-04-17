4d1d0374bf9dbb2639d7e7073a873950fbbe433f
vbetsch <vic.betsch@gmail.com>
2026-04-17 19:21:25 +0200

    test: addressLinks

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,13 @@
+import { addressLinks } from '@data/address-links.ts';
+
 describe('config', () => {
   it('should be OK', () => {
     expect(true).toBeTruthy();
+  });
+  it('content > addressLinks - should be defined', () => {
+    expect(true).toBeTruthy();
+    expect(addressLinks).toBeDefined();
+    expect(addressLinks).toBeTruthy();
+    expect(addressLinks).toHaveLength(4);
   });
 });
