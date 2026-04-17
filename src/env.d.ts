956e847b4a26fcc0b0cbf09183515a5b498a40fe
vbetsch <vic.betsch@gmail.com>
2026-04-17 19:43:17 +0200

    Revert "test: delete env.d.ts"
    This reverts commit dca0df53ce2cdebfbaf5be2b64020ec781f1971a.

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+type ImportMetaEnv = {
+  readonly ID_GITHUB: string;
+  readonly ID_LINKEDIN: string;
+  readonly MAILTO_EMAIL: string;
+  readonly PHONE_NUMBER: string;
+}
+
+type ImportMeta = {
+  readonly env: ImportMetaEnv;
+}
