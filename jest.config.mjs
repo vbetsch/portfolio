6644ebb8cecb927e0b54ab54a7572cb6526c73a8
vbetsch <vic.betsch@gmail.com>
2026-05-29 19:12:37 +0200

    fix: rename @types to @t because conflict

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,7 +13,7 @@ export default {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@data/(.*)$': '<rootDir>/src/data/$1',
-    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@t/(.*)$': '<rootDir>/src/types/$1',
     '^astro:env/client$': '<rootDir>/tests/mocks/astro-env.ts',
   },
   transform: {
