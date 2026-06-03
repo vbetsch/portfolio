9b6275aa4e17068c5e865cf1960aa28e311494d7
vbetsch <vic.betsch@gmail.com>
2026-06-03 19:46:06 +0200

    format: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,24 +4,24 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    globals: true,
-    environment: 'node',
     alias: {
       'astro:env/client': new URL('./tests/mocks/astro-env.ts', import.meta.url).pathname,
     },
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.ts'],
       exclude: [
         'node_modules/**',
         'src/components/**',
         'src/data/**',
-        'src/env.d.ts',
         'src/pages/**',
+        'src/env.d.ts',
         'tests/mocks/**',
         'vitest.config.ts',
       ],
+      include: ['src/**/*.ts'],
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
     },
+    environment: 'node',
+    globals: true,
   },
 });
