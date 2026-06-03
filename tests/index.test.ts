f61512d8e7b93cb4e823827a9912881d11929287
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-06-03 19:58:40 +0200

    test: Get full coverage in unit tests (#65)
    * feat: exclude enums and types
    
    * refactor: remove addressLinks test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,5 @@
-import { addressLinks } from '@data/address-links.ts';
-
 describe('config', () => {
   it('should be OK', () => {
     expect(true).toBeTruthy();
-  });
-  it('content > addressLinks - should be defined', () => {
-    const addressLinksNumber: number = 4;
-    expect(addressLinks).toBeDefined();
-    expect(addressLinks).toBeTruthy();
-    expect(addressLinks).toHaveLength(addressLinksNumber);
   });
 });
