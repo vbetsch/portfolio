513f3291fb4a4931c5db7d0f76a7871c6ff02a72
vbetsch <vic.betsch@gmail.com>
2026-06-03 20:48:19 +0200

    fix: vitest config scope

--- vitest.config.ts
+++ vitest.config.ts
@@ -24,6 +24,8 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
     },
     environment: 'node',
+    exclude: ['tests/e2e/**', 'node_modules/**', 'dist/**'],
     globals: true,
+    include: ['tests/units/**/*.test.ts'],
   },
 });
