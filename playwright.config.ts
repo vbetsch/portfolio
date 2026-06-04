0b20dd429a7be062a24bab085a6912cffa17a7b9
vbetsch <vic.betsch@gmail.com>
2026-06-04 17:07:57 +0200

    fix: use import.meta.dirname

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(import.meta.dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests/e2e',
