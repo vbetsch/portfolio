da7a8642d3e974a957fc60a8f07fecee5cd2e9b9
vbetsch <vic.betsch@gmail.com>
2026-06-04 17:23:08 +0200

    test: should have a Contact Me CTA button

--- tests/e2e/main.spec.ts
+++ tests/e2e/main.spec.ts
@@ -2,10 +2,12 @@ import { type Locator, test, expect } from '@playwright/test';
 
 test.describe('Hero', () => {
   let mainSection: Locator;
+  let contactMeCTAButton: Locator;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     mainSection = page.getByRole('main');
+    contactMeCTAButton = mainSection.getByRole('link', { name: 'Me contacter' });
   });
 
   test('should have the role as first title', async () => {
@@ -16,5 +18,9 @@ test.describe('Hero', () => {
   test('should have the status as second title', async () => {
     const secondTitle = mainSection.locator('h2');
     await expect(secondTitle).toContainText('Recherche un poste en tant que développeur web');
+  });
+
+  test('should have a Contact Me CTA button', async () => {
+    await expect(contactMeCTAButton).toBeVisible();
   });
 });
