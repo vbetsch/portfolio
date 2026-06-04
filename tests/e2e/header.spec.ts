eb4e1a730c5a55f28b79823d149ec5f485ba2c73
vbetsch <vic.betsch@gmail.com>
2026-06-04 19:36:05 +0200

    refactor: rename contactMeButton to contactMeElement

--- tests/e2e/header.spec.ts
+++ tests/e2e/header.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Navbar', () => {
   let header: Locator;
-  let contactMeButton: Locator;
+  let contactMeElement: Locator;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     header = page.locator('#header');
-    contactMeButton = header.getByRole('link', { name: 'Me contacter' });
+    contactMeElement = header.getByRole('link', { name: 'Me contacter' });
   });
 
   test('should have the fullname', async () => {
@@ -16,12 +16,12 @@ test.describe('Navbar', () => {
   });
 
   test('should have a Contact Me element', async () => {
-    await expect(contactMeButton).toBeVisible();
+    await expect(contactMeElement).toBeVisible();
   });
 
   test.describe('Contact Me', () => {
     test('should be a mailto link to the mail address', async () => {
-      await expect(contactMeButton).toHaveAttribute('href', `mailto:${process.env.MAILTO_EMAIL}`);
+      await expect(contactMeElement).toHaveAttribute('href', `mailto:${process.env.MAILTO_EMAIL}`);
     });
   });
 });
