587fb8b4f6b63bea44110c0f4cfac497db5256c9
vbetsch <vic.betsch@gmail.com>
2026-06-04 16:30:40 +0200

    refactor: disable home spec

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,24 +1,24 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Home page - Portfolio', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('should display right title', async ({ page }) => {
-    await expect(page).toHaveTitle(/Victor BETSCH/);
-  });
-
-  test('should have a functional link to Github', async ({ page }) => {
-    const githubLink = page.locator('a[href*="github.com"]');
-    await expect(githubLink).toBeVisible();
-  });
-
-  test('contact button should point to mailto', async ({ page }) => {
-    const mainSection = page.getByRole('main');
-    const contactButton = mainSection.getByRole('link', { name: 'Me contacter' });
-
-    await expect(contactButton).toBeVisible();
-    await expect(contactButton).toHaveAttribute('href', 'mailto:vbetsch.contact@gmail.com');
-  });
-});
+// import { test, expect } from '@playwright/test';
+//
+// test.describe('Home page - Portfolio', () => {
+//   test.beforeEach(async ({ page }) => {
+//     await page.goto('/');
+//   });
+//
+//   test('should display right title', async ({ page }) => {
+//     await expect(page).toHaveTitle(/Victor BETSCH/);
+//   });
+//
+//   test('should have a functional link to Github', async ({ page }) => {
+//     const githubLink = page.locator('a[href*="github.com"]');
+//     await expect(githubLink).toBeVisible();
+//   });
+//
+//   test('contact button should point to mailto', async ({ page }) => {
+//     const mainSection = page.getByRole('main');
+//     const contactButton = mainSection.getByRole('link', { name: 'Me contacter' });
+//
+//     await expect(contactButton).toBeVisible();
+//     await expect(contactButton).toHaveAttribute('href', 'mailto:vbetsch.contact@gmail.com');
+//   });
+// });
