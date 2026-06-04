116064c8ce766bf366e65e22db778e4c3f7f35ea
vbetsch <vic.betsch@gmail.com>
2026-06-04 19:14:53 +0200

    test: should have the global css import

--- tests/e2e/head.spec.ts
+++ tests/e2e/head.spec.ts
@@ -4,3 +4,9 @@ test('should have the fullname as website title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/Victor BETSCH/);
 });
+
+test('should have the global css import', async ({ page }) => {
+  await page.goto('/');
+  const stylesheetLink = page.locator('link[rel="stylesheet"][href*="global"]');
+  await expect(stylesheetLink).toBeAttached();
+});
