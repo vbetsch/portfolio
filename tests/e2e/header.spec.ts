19a3e13a47f8d3343b46d32beb9f2463f5f95ff5
vbetsch <vic.betsch@gmail.com>
2026-06-04 16:58:33 +0200

    refactor: reduce test lines

--- tests/e2e/header.spec.ts
+++ tests/e2e/header.spec.ts
@@ -1,28 +1,26 @@
+import type { Locator } from '@playwright/test';
 import { test, expect } from '@playwright/test';
 
 test.describe('Navbar', () => {
+  let header: Locator;
+  let contactMeButton: Locator;
+
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    header = page.locator('#header');
+    contactMeButton = header.getByRole('link', { name: 'Me contacter' });
   });
 
-  test('should have the fullname as title', async ({ page }) => {
-    const header = page.locator('#header');
-    await expect(header).toBeVisible();
+  test('should have the fullname as title', async () => {
     await expect(header).toContainText('Victor BETSCH');
   });
 
-  test('should have a Contact Me button', async ({ page }) => {
-    const header = page.locator('#header');
-    await expect(header).toBeVisible();
-    const contactMeButton = header.getByRole('link', { name: 'Me contacter' });
+  test('should have a Contact Me button', async () => {
     await expect(contactMeButton).toBeVisible();
   });
 
   test.describe('Contact Me', () => {
-    test('should be a mailto link to the mail address', async ({ page }) => {
-      const header = page.locator('#header');
-      await expect(header).toBeVisible();
-      const contactMeButton = header.getByRole('link', { name: 'Me contacter' });
+    test('should be a mailto link to the mail address', async () => {
       await expect(contactMeButton).toHaveAttribute('href', 'mailto:vbetsch.contact@gmail.com');
     });
   });
