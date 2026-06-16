1bc2f1e304ddb40561a50fb8f3ed83ac5ffc9e1d
vbetsch <vic.betsch@gmail.com>
2026-06-16 16:09:59 +0200

    Revert "perf: set 2 workers for playwright"
    This reverts commit 0f6a915b45907dfb2c59792126aa15cda3b9a0ed.

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  ...(process.env.CI ? { workers: 2 } : {}),
+  ...(process.env.CI ? { workers: 1 } : {}),
   reporter: 'html',
 
   use: {
