0f6a915b45907dfb2c59792126aa15cda3b9a0ed
vbetsch <vic.betsch@gmail.com>
2026-06-16 16:06:45 +0200

    perf: set 2 workers for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  ...(process.env.CI ? { workers: 1 } : {}),
+  ...(process.env.CI ? { workers: 2 } : {}),
   reporter: 'html',
 
   use: {
