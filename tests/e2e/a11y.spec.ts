8686efe0278f80bdd87270e9c1f19bcde9b95561
vbetsch <vic.betsch@gmail.com>
2026-06-18 17:47:54 +0200

    feat: improve a11y tests

--- tests/e2e/a11y.spec.ts
+++ tests/e2e/a11y.spec.ts
@@ -3,7 +3,13 @@ import AxeBuilder from '@axe-core/playwright';
 
 test('The homepage complies with WCAG standards', async ({ page }) => {
   await page.goto('/');
-  const results = await new AxeBuilder({ page }).analyze();
-  console.log('results', results);
-  expect(results.violations).toEqual([]);
+  const scanResults = await new AxeBuilder({ page }).analyze();
+
+  if (scanResults.violations.length > 0) {
+    const errorLog = scanResults.violations.map(violation =>
+      `❌ Rule: ${violation.id} (${violation.help})\n   Impact: ${violation.impact}\n   Elements concerned: ${violation.nodes.map(node => node.target).join(', ')}`
+    ).join('\n\n');
+
+    throw new Error(`Accessibility violations detected :\n\n${errorLog}`);
+  }
 });
