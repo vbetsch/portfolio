6d232c74b05a26a0d1d99a0a1edf9cd6594dafc1
vbetsch <vic.betsch@gmail.com>
2026-06-25 14:33:02 +0200

    refactor: create e2e tags enum

---
+++ tests/e2e/e2e-tags.enum.ts
@@ -0,0 +1,10 @@
+export enum E2eTagsEnum {
+  ACCESSIBILITY = 'a11y',
+  ASSETS = 'assets',
+  HEALTHCHECK = 'healthcheck',
+  MAIN_TITLE = 'main-title',
+  ROOT_ELEMENTS = 'root-elements',
+  SMOKE_LEVEL_1 = 'smoke-lv1',
+  SMOKE_LEVEL_2 = 'smoke-lv2',
+  SMOKE_LEVEL_3 = 'smoke-lv3',
+}
