56159075f0342b2b6fe5bf11b3ea3e67520b60a4
vbetsch <vic.betsch@gmail.com>
2026-06-25 14:52:08 +0200

    refactor: use e2e tags in a11y

--- tests/e2e/app/a11y.spec.ts
+++ tests/e2e/app/a11y.spec.ts
@@ -1,18 +1,23 @@
 import { test } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
+import { E2eTagsEnum } from '../e2e-tags.enum.ts';
 
-test('The homepage complies with WCAG standards @a11y @smoke-lv3', async ({ page }) => {
-  await page.goto('/');
-  const scanResults = await new AxeBuilder({ page }).analyze();
+test(
+  'The homepage complies with WCAG standards',
+  { tag: [E2eTagsEnum.ACCESSIBILITY, E2eTagsEnum.SMOKE_LEVEL_3] },
+  async ({ page }) => {
+    await page.goto('/');
+    const scanResults = await new AxeBuilder({ page }).analyze();
 
-  if (scanResults.violations.length > 0) {
-    const errorLog = scanResults.violations
-      .map(
-        violation =>
-          `❌ Rule: ${violation.id} (${violation.help})\n   Impact: ${violation.impact}\n   Elements concerned: ${violation.nodes.map(node => node.target).join(', ')}`
-      )
-      .join('\n\n');
+    if (scanResults.violations.length > 0) {
+      const errorLog = scanResults.violations
+        .map(
+          violation =>
+            `❌ Rule: ${violation.id} (${violation.help})\n   Impact: ${violation.impact}\n   Elements concerned: ${violation.nodes.map(node => node.target).join(', ')}`
+        )
+        .join('\n\n');
 
-    throw new Error(`Accessibility violations detected :\n\n${errorLog}`);
+      throw new Error(`Accessibility violations detected :\n\n${errorLog}`);
+    }
   }
-});
+);
