51f4ef8132437021d8c5c4d71553ede1069a913d
vbetsch <vic.betsch@gmail.com>
2026-06-25 15:26:33 +0200

    feat: create smoke-lv2 tests

---
+++ tests/e2e/app/assets.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+import { E2ETagsEnum } from '@tests/e2e/e2e-tags.enum';
+
+test.describe(
+  'Assets Integrity',
+  {
+    tag: [E2ETagsEnum.SMOKE_LEVEL_2, E2ETagsEnum.ASSETS],
+  },
+  () => {
+    test(
+      'Favicon - Should return HTTP 200 and correct Content-Type',
+      { tag: [E2ETagsEnum.FAVICON] },
+      async ({ request }) => {
+        const response = await request.get('/favicon.ico');
+        expect(response.status()).toBe(200);
+        expect(response.headers()['content-type']).toContain('image/');
+      }
+    );
+
+    test(
+      'CV PDF - Should return HTTP 200 and application/pdf',
+      { tag: [E2ETagsEnum.PDF_FILE] },
+      async ({ request }) => {
+        const response = await request.get(`/files/cv_fr.pdf`);
+        expect(response.status()).toBe(200);
+        expect(response.headers()['content-type']).toContain('application/pdf');
+      }
+    );
+
+    test(
+      'Compiled CSS - Should exist in DOM and return HTTP 200',
+      { tag: [E2ETagsEnum.CSS_FILE] },
+      async ({ request }) => {
+        const homeResponse = await request.get('/');
+        expect(homeResponse.status()).toBe(200);
+        const html = await homeResponse.text();
+
+        const cssMatch = html.match(
+          /<link[^>]+rel=["']stylesheet["'][^>]+href=["'](\/_astro\/[^"']+\.css)["']/i
+        );
+        expect(cssMatch, 'The link tag from the Astro CSS cannot be found in the DOM').not.toBeNull();
+
+        if (cssMatch) {
+          const cssUrl = cssMatch[1];
+
+          const cssResponse = await request.get(cssUrl);
+          expect(cssResponse.status()).toBe(200);
+          expect(cssResponse.headers()['content-type']).toContain('text/css');
+        }
+      }
+    );
+  }
+);
