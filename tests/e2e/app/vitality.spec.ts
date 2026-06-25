bce9b44ff62eed44cbdfe0c374e90206050bcf73
vbetsch <vic.betsch@gmail.com>
2026-06-25 14:37:26 +0200

    refactor: use http-status-codes

--- tests/e2e/app/vitality.spec.ts
+++ tests/e2e/app/vitality.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+import {StatusCodes} from "http-status-codes";
 
 test.describe('Vitality Check @smoke-lv1', () => {
   test('Healthcheck - Should return HTTP 200 connectivity @healthcheck', async ({ request }) => {
     const response = await request.get('/');
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(StatusCodes.OK);
   });
 
   test('Root DOM Elements - Should mount root elements @root-elements', async ({ page }) => {
     const response = await page.goto('/');
-    expect(response?.status()).toBe(200);
+    expect(response?.status()).toBe(StatusCodes.OK);
 
     const mainElement = page.locator('main');
     await expect(mainElement).toBeVisible();
